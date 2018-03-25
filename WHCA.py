b8d9a8f6f7d740fc525eef9590212c6a9aedfe88
Anton Stagge <anton.stagge@winfallvr.com>
2018-03-25 18:11:47 +0200

    Added first attempt on WHCA

--- WHCA.py
+++ WHCA.py
@@ -9,12 +9,15 @@ def manhattan_distance(start, end):
     dist_y = abs(start.coordinates[0] - end.coordinates[0])
     return dist_x + dist_y
 
-def AStar(graph, start, target):
+def hash(a, b):
+    return int((a+b)*(a+b+1)/2 + b)
+
+def findPathAStar(graph, start, target, rTable, W):
     start.g = 0
     start.h = manhattan_distance(start, target)
     start.f = start.h
 
-    neighbours = [(0,1),(1,0),(-1,0),(0,-1)]
+    neighbours = [(0,1),(1,0),(-1,0),(0,-1), (0,0)]
 
     open_list = []
     closed_list = set()
@@ -45,6 +48,10 @@ def AStar(graph, start, target):
             if neighbour.type == NodeType.OBSTACLE or neighbour in closed_list:
                 continue
 
+            key = hash(current.g + 1, neighbour.id)
+            if key in rTable:
+                continue
+
             if neighbour not in open_list or current.g + 1 < neighbour.g:
                 neighbour.g = current.g + 1
                 neighbour.h = manhattan_distance(neighbour, target)
@@ -53,11 +60,55 @@ def AStar(graph, start, target):
                 if neighbour not in open_list:
                     heappush(open_list, neighbour)
 
+def WHCA(graph, agents, W, K):
+    rTable = dict()
+    while True:
+        done = True
+        for a in agents:
+            if not a.reachedGoal:
+                done = False
+                break
+        if done:
+            break
+
+        for a in agents:
+            for i in range(0, graph.shape[0]):
+                for j in range(0, graph.shape[1]):
+                    graph[i][j].g = None
+                    graph[i][j].h = None
+                    graph[i][j].f = None
+                    graph[i][j].came_from = None
+            path = findPathAStar(graph, a.pos, a.goal,rTable, W)
+            for t, value in enumerate(path):
+                if t < W:
+                    rTable[hash(t, value.id)] = True
+            a.path = path
+
+        for a in agents:
+            a.move(K)
+
+        rTable = dict()
+
+
+
+class Agent(object):
+    def __init__(self, pos, goal, id):
+        self.id = id
+        self.reachedGoal = pos == goal
+        self.pos = pos
+        self.goal = goal
+        self.path = None
+
+    def move(self, steps):
+        self.pos = self.path[steps]
+        if self.pos == self.goal:
+            self.reachedGoal = True
+
 
 
 g = create_Astar_graph(wh)
 start = g[1][0]
-target = g[3][3]
+target = g[4][3]
+agent_list = [Agent(g[1][0], g[4][3], 1337), Agent(g[0][0], g[3][3], 69)]
 
-path = AStar(g, start, target)
-print([x.id for x in path])
+WHCA(g, agent_list, 5, 3)
