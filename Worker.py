e30de1307cef8ef0a6568d4034cdf4ae0f779731
Cristian <cristian.osoriobretti@gmail.com>
2018-04-06 17:20:14 +0200

    Done with order distribution

--- Worker.py
+++ Worker.py
@@ -1,6 +1,7 @@
 class Worker(object):
-	def __init__(self):
+	def __init__(self, new_id):
 		self.items = []
+		self.id = new_id
 
 	def add_order(order):
 		items = items + order
--- main.py
+++ main.py
@@ -1,36 +1,55 @@
 from orders import small_order_list as small
 from warehouse import warehouse
 from create_graphs import *
-from Agent import * 
+from Agent import *
+from Worker import *
 from WHCA import *
 from draw_simulation import draw
 
-def get_correct_node(items, node_id):
-	for current in items:
+def get_correct_node(pickup_nodes, node_id):
+	for current in pickup_nodes:
 		if current.id == node_id:
 			return current
-	raise Exception('Could not find id:%d in the list of items' %(node_id))
+	raise Exception('Could not find id:%d in the list of pickup_nodes' %(node_id))
 
 
-def generate_order_list(graph, items, workers):
-	for order in small:
-		for node_id in order:
-			node = get_correct_node(items, node_id) 
+def create_workers(drop_off_nodes):
+	workers = [Worker(new_id) for new_id, x in enumerate(drop_off_nodes)]
 
+def create_orders(order_input, pickup_nodes):
+	orders = []
+	for order_list in order_input:
+		order = []
+		for node_id in order_list:
+			order.append(get_correct_node(pickup_nodes, node_id))
+		orders.append(order)
+
+def distribute_orders(workers, orders):
+	worker_number = 0
+	while orders:
+		workers[worker_number].add_order(orders.pop(0))
+		worker_number = (worker_number + 1) % len(workers)
 
 
 def main():
-	g, items, workers = create_Astar_graph(warehouse)
-	agent_list = [Agent(g[1][0], g[15][25], 1, None), Agent(g[15][25], g[1][0], 2, None)]
+	order_input = small
+	number_of_agents = 5
 
-    #workers = generate_order_list(g, items, workers)
+	graph, pickup_nodes, drop_off_nodes = create_Astar_graph(warehouse)
+
+	workers = create_workers(drop_off_nodes)
+	orders = create_orders(order_input, pickup_nodes)
+	distribute_orders(workers, orders)
+
+
+	agent_list = [Agent(graph[1][0], graph[15][25], 1, None), Agent(graph[15][25], graph[1][0], 2, None)]
     #for a in agent_list:
     #   print("Agent %d starts at %d and wants to get to %d" % (a.id, a.pos.id, a.goal.id))
 
-	WHCA(g, agent_list, 10, 5)
+	WHCA(graph, agent_list, 10, 5)
 
 
-	draw(agent_list, g)
+	draw(agent_list, graph)
 
 	for i in range(0, len(agent_list[0].walking_path)):
 		if agent_list[0].walking_path[i] == agent_list[1].walking_path[i]:
